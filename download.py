0fb21eaafc6e61fbba2f7e09f3414006fca56005
MaxHerrera <max.herrera@correo.usa.edu.co>
2017-11-11 09:19:51 -0500

    modificacion del rango del metodo

--- download.py
+++ download.py
@@ -3,7 +3,7 @@ import random
 
 
 def downloader(image_url):
-    file_name = random.randrange(1,10000)
+    file_name = random.randrange(1,100)
     full_file_name = str(file_name) + '.jpg'
     urllib.request.urlretrieve(image_url,full_file_name)
 
